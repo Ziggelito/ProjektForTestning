059f7a58d2646463426fdc31c7d05440257bcb78
Ziggelito <Sixten.Lonnberg@yh.nackademin.se>
2023-09-29 10:50:05 +0200

    Initial commit

---
+++ src/main/java/com/example/projektfortestning/ProjektForTestningApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektfortestning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektForTestningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektForTestningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projektfortestning/ProjektForTestningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektfortestning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektForTestningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
